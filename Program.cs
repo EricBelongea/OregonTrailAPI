1b4aaa995fa769aafa1f5dd8d192d4440e9da659
Eric Belongea <belongeae@infinity-software.com>
2024-04-25 07:46:40 -0600

    Create Item, Food, and PassengerFood classes and updated the db relationships.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OregonTrailAPI.Context.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CaravanContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OregonTrail"))
+           .EnableSensitiveDataLogging()
+           .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 
 var app = builder.Build();
 
